e185c876d010a1d79e125d9c33a1dcfde414f009
vyou <van-rottana.you@hotmail.com>
2022-02-08 16:56:22 +0100

    ajout navbar

--- flagapp/src/App.js
+++ flagapp/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { LoginPage } from './LoginPage';
-
+import NavBar from './_components/NavBar/NavBar';
 
 function App() {
     const alert = useSelector(state => state.alert);
@@ -16,16 +16,19 @@ function App() {
     //}, []);
 
     return (
-        <div className="jumbotron">
-            <div className="container">
-                <div className="col-md-8 offset-md-2">
-                    {alert.message &&
-                        <div className={`alert ${alert.type}`}>{alert.message}</div>
-                    }
-                    <Routes>
-                        <Route exact path="/login" element={<LoginPage />} />
-                        <Route exact path='/' element={<LoginPage />} />
-                    </Routes>
+        <div>
+           <NavBar />
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-md-8 offset-md-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Routes>
+                            <Route exact path="/login" element={<LoginPage />} />
+                            <Route exact path='/' element={<LoginPage />} />
+                        </Routes>
+                    </div>
                 </div>
             </div>
         </div>
