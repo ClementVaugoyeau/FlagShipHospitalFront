372f27566ee385acb05206f94ad308ada208c0ad
ClementVaugoyeau <77982870+ClementVaugoyeau@users.noreply.github.com>
2022-02-09 16:05:52 +0100

    Update CreatePatient

--- flagapp/src/CreatePatient/CreatePatient.jsx
+++ flagapp/src/CreatePatient/CreatePatient.jsx
@@ -4,53 +4,68 @@ import Table from 'react-bootstrap/Table'
 
 import axios from 'axios';
 
-export default function CreatePatient()  {
-    
-    
-   
-   
+export default function CreatePatient() {
+  const [UserID, setUserID] = useState('');
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [dateArrivee, setDateArrivee] = useState('');
+  const [dateDepart, setDateDepart] = useState('');
+  const [note, setNote] = useState();
+
+  const postData = () => {
+    axios.post('https://localhost:7008/api/Dossierpatient', {
+      UserID,
+      nom,
+      prenom,
+      dateArrivee,
+      dateDepart,
+      note
+    })
+  }
+
+
     return (
-        <div className="container">
+      <div className="container">
 
-            <h1 id="titleTable" >Création du dossier patient</h1>
-            
-<Table striped bordered hover>
-  
-  <tbody>
-    <tr>
-      <td>Prénom</td>
-      <td><input placeholder='Prénom'></input></td>
-     
-    </tr>
-    <tr>
-      <td>Nom</td>
-      <td><input placeholder='Nom'></input></td>
-      
-    </tr>
-    <tr>
-      <td>Date d'arrivée</td>
-      <td ><input type="datetime-local" id="arrivee" name="arrivee"></input></td>
-      
-    </tr>
-    <tr>
-      <td>Date de départ</td>
-      <td ><input type="datetime-local" id="depart" name="depart"></input></td>
-      
-    </tr>
-    <tr>
-      <td>Note concernant le Patient</td>
-      <td ><textarea rows="9" cols="40" placeholder="Symptômes, diagnotics traitements, divers remarques pour l'equipe medicale."></textarea></td>
-      
-    </tr>
-    
-  </tbody>
-</Table>
-    
-    <button className='btn-primary m-2 rounded'  >Enregistrer</button>
-        </div>
+        <h1 id="titleTable" >Création du dossier patient</h1>
+
+        <Table striped bordered hover>
+
+          <tbody>
+            <tr>
+              <td>Prénom</td>
+              <td><input placeholder='Prénom' value={prenom} onChange={(e) => setPrenom(e.target.value)}></input></td>
+
+            </tr>
+            <tr>
+              <td>Nom</td>
+              <td><input placeholder='Nom' value={nom} onChange={(e) => setNom(e.target.value)}></input></td>
+
+            </tr>
+            <tr>
+              <td>Date d'arrivée</td>
+              <td ><input type="datetime-local" id="arrivee" name="arrivee" value={dateArrivee} onChange={(e) => setDateArrivee(new Date(e.target.value))}></input></td>
+
+            </tr>
+            <tr>
+              <td>Date de départ</td>
+              <td ><input type="datetime-local" id="depart" name="depart" value={dateDepart} onChange={(e) => setDateDepart(new Date(e.target.value))}></input></td>
+
+            </tr>
+            <tr>
+              <td>Note concernant le Patient</td>
+              <td ><textarea rows="9" cols="40" placeholder="Symptômes, diagnotics traitements, divers remarques pour l'equipe medicale."
+              value={note} onChange={(e) => setNote(e.target.value)}></textarea></td>
+
+            </tr>
+
+          </tbody>
+        </Table>
+
+        <button className='btn-primary m-2 rounded' onClick={postData} type='submit' >Enregistrer</button>
+      </div>
     )
-    
 
 
 
-}
+  }
